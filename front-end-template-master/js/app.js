885db12949780336b052ff7cb6118d2e4913b955
D. Sean Bailey <dseanbailey@gmail.com>
2018-03-29 23:16:55 -0400

    Update app.js

---
+++ front-end-template-master/js/app.js
@@ -0,0 +1,5 @@
+/* This is the JavaScript Which Loads Atomatically When Someone Visits Louisville MUSICulture's Webpage. It's not much, just a simle prompt/CTA to try and get the end user involved. --------------------- */
+
+
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
